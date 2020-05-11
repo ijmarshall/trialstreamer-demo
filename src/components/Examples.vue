c90b5fd26bf07c43a7f97f5ebeb3e15aa01ac04f
Iain Marshall <mail@ijmarshall.com>
2020-05-11 10:13:08 +0200

    edited examples

--- src/components/Examples.vue
+++ src/components/Examples.vue
@@ -4,7 +4,7 @@
     <b-col>
       <p class=" lead" >Try some of these examples:</p>
       <ul>
-        <li><router-link :to="{path: '/', query: {q: example1}}">What are the effects of antivirals for treating COVID-19?</router-link></li>
+        <li><router-link :to="{path: '/', query: {q: example1}}">What is the effect of remdesivir for treating COVID-19?</router-link></li>
         <li><router-link :to="{path: '/', query: {q: example2}}">What is the best ventilation strategy for acute respiratory distress syndrome (ARDS)?</router-link></li>
         <li><router-link :to="{path: '/', query: {q: example3}}">Do chloroquine or hydroxychloroquine improve outcomes in COVID-19 infection?</router-link></li>
         <li><router-link :to="{path: '/', query: {q: example4}}">What trials are underway on the use of Extracorporeal Membrane Oxygenation (ECMO) in COVID-19?</router-link></li>
@@ -57,7 +57,7 @@ import JSURL from "jsurl";
 export default {
   data() {
     return {
-      example1: JSURL.stringify([{"field":"population", "text": "COVID-19 [population]", "cui":"TS-COV19"}, {"field": "interventions", "text": "Antiviral agents [interventions]", "cui": "C0003451"}]),
+      example1: JSURL.stringify([{"field":"population", "text": "COVID-19 [population]", "cui":"TS-COV19"}, {"field": "interventions", "text": "Remdesivir [interventions]", "cui": "C4726677"}]),
       example2: JSURL.stringify([{"field":"population", "text": "Acute Respiratory Distress Syndrome [population]", "cui":"C0035222"}, {"field": "interventions", "text": "Ventilation [interventions]", "cui": "C0554804"}]),
       example3: JSURL.stringify([{"field":"population", "text": "COVID-19 [population]", "cui":"TS-COV19"}, {"field": "interventions", "text": "Chloroquine [interventions]", "cui": "C0008269"}]),
       example4: JSURL.stringify([{"field":"population", "text": "COVID-19 [population]", "cui":"TS-COV19"}, {"field": "interventions", "text": "Extracorporeal Membrane Oxygenation (ECMO) [interventions]", "cui": "C0015357"}]),
